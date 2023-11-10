53153efc3c5869afccd0c93be1e92382803902c0
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2023-11-10 02:39:15 -0500

    Add files via upload

--- Hand.cpp
+++ Hand.cpp
@@ -1,40 +1,60 @@
 #include "Hand.hpp"
+#include <string>
+#include <algorithm>
 
-Hand::Hand() {}
+Hand::Hand() : cards_()
+{
 
-Hand::~Hand() {}
+}
 
-Hand::Hand(const Hand& other) : cards_(other.cards_) {}
+Hand::~Hand()
+{
+    cards_.clear();
+}
 
-Hand& Hand::operator=(const Hand& other) {
-    if (this != &other) {
+Hand::Hand(const Hand &other) : cards_(other.cards_)
+{
+
+}
+
+Hand &Hand::operator=(const Hand &other)
+{
+    if(this != &other)
         cards_ = other.cards_;
-    }
     return *this;
 }
 
-Hand::Hand(Hand&& other) : cards_(std::move(other.cards_)) {}
+Hand::Hand(Hand &&other) : cards_(std::move(other.cards_))
+{
+    
+}
 
-Hand& Hand::operator=(Hand&& other) {
-    if (this != &other) {
+Hand &Hand::operator=(Hand &&other)
+{
+    if(this != & other){
         cards_ = std::move(other.cards_);
     }
     return *this;
 }
 
-const std::deque<PointCard>& Hand::getCards() const {
+const std::deque<PointCard> &Hand::getCards() const
+{
     return cards_;
 }
 
-void Hand::addCard(PointCard&& card) {
-    cards_.emplace_back(std::move(card));
+void Hand::addCard(PointCard &&card)
+{
+    card.setDrawn(true);
+    cards_.push_back(card);
 }
 
-bool Hand::isEmpty() const {
+bool Hand::isEmpty() const
+{
     return cards_.empty();
 }
 
-void Hand::Reverse() {
+void Hand::Reverse()
+{
     std::reverse(cards_.begin(), cards_.end());
 }
 
@@ -44,11 +64,14 @@ int Hand::PlayCard()
         throw std::exception();
     }
     PointCard card = cards_.front();
+
     if(!card.isPlayable()){
         cards_.pop_front();
         throw std::exception();
     }
+
     int ret = std::stoi(card.getInstruction());
+
     cards_.pop_front();
     return ret;
 }
